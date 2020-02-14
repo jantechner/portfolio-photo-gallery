394f7ee99742e5f864546ab6fb432ca43b5f07a7
Jan Techner <jantechner@live.com>
2020-02-14 16:18:01 +0100

    Add sample images

---
+++ src/images/paintings.js
@@ -0,0 +1,35 @@
+const paintings = [
+    {
+        id: 1,
+        image: require('./sample4.jpg').default,
+        title: "Sample 1"
+    },
+    {
+        id: 2,
+        image: require('./sample2.png').default,
+        title: "Sample 2"
+    },
+    {
+        id: 3,
+        image: require('./sample3.jpg').default,
+        title: "Sample 3"
+    },
+    {
+        id: 4,
+        image: require('./sample4.jpg').default,
+        title: "Sample 4"
+    },
+    {
+        id: 5,
+        image: require('./sample4.jpg').default,
+        title: "Sample 5"
+    },
+    {
+        id: 6,
+        image: require('./sample2.png').default,
+        title: "Sample 6"
+    },
+ 
+];
+
+export default paintings;
