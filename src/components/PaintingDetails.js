f9ae7eae8fb6dd8011ea002f6fb1664bce0cb62d
Jan Techner <jantechner@live.com>
2020-02-15 12:17:12 +0100

    Add navigation between paintings

--- src/components/PaintingDetails.js
+++ src/components/PaintingDetails.js
@@ -1,11 +1,25 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import Paintings from "./../images/paintings";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./PaintingDetails.scss";
+
+function getAdjacentPaintingsId(currentId) {
+  let index = Paintings.findIndex(p => p.id === currentId);
+  const previous = Paintings[index === 0 ? index : index - 1].id;
+  const next = Paintings[index === Paintings.length - 1 ? index : index + 1].id;
+  return [previous, next];
+}
 
 function PaintingDetails(props) {
   let { paintingId } = useParams();
-  let painting = Paintings.find(painting => painting.id.toString() === paintingId);
+  let painting = Paintings.find(
+    painting => painting.id.toString() === paintingId
+  );
+
+  const [previousPaintingId, nextPaintingId] = getAdjacentPaintingsId(
+    painting.id
+  );
 
   if (!painting) {
     return <h1>No such painting</h1>;
@@ -14,7 +28,23 @@ function PaintingDetails(props) {
       <div className="painting-details">
         <h1>Painting {painting.id}</h1>
         <h2>Title: {painting.title}</h2>
-        <img src={painting.image} alt={painting.title} />
+        <div className="image-navigation-wrapper">
+          <Link to={previousPaintingId.toString()}>
+            <FontAwesomeIcon
+              icon={["fas", "chevron-left"]}
+              size="3x"
+              style={{ color: "gray" }}
+            />
+          </Link>
+          <img src={painting.image} alt={painting.title} />
+          <Link to={nextPaintingId.toString()}>
+            <FontAwesomeIcon
+              icon={["fas", "chevron-right"]}
+              size="3x"
+              style={{ color: "gray" }}
+            />
+          </Link>
+        </div>
       </div>
     );
   }
