ef26b7d24bf406f46449752c360169a343ac60ee
Jan Techner <jantechner@live.com>
2020-02-19 02:57:10 +0100

    Change PaintingDetails to Component

--- src/components/PaintingDetails.js
+++ src/components/PaintingDetails.js
@@ -1,80 +1,87 @@
-import React from "react";
-import { Link, useParams } from "react-router-dom";
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import Paintings from "./../images/paintings";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./PaintingDetails.scss";
 
-function getAdjacentPaintingsId(currentId) {
-  let index = Paintings.findIndex(p => p.id === currentId);
-  const previous = Paintings[index === 0 ? index : index - 1].id;
-  const next = Paintings[index === Paintings.length - 1 ? index : index + 1].id;
-  return [previous, next];
-}
+class PaintingDetails extends Component {
+  constructor(props) {
+    super(props);
+    const paintingId = this.props.match.params.paintingId;
+    this.painting = Paintings.find(p => p.id.toString() === paintingId);
+    const [previous, next] = this.getAdjacentPaintingsId(paintingId);
+    this.previous = previous;
+    this.next = next;
+    this.state = { paintingId: paintingId };
+  }
 
-function PaintingDetails(props) {
-  let { paintingId } = useParams();
-  let painting = Paintings.find(
-    painting => painting.id.toString() === paintingId
-  );
+  componentDidUpdate(prevProps) {
+    if (
+      this.props.match.params.paintingId !== prevProps.match.params.paintingId
+    ) {
+      const paintingId = this.props.match.params.paintingId;
+      this.painting = Paintings.find(p => p.id.toString() === paintingId);
+      const [previous, next] = this.getAdjacentPaintingsId(paintingId);
+      this.previous = previous;
+      this.next = next;
+      this.setState({paintingId: paintingId});
+    }
+  }
 
-  const renderLeftArrow = previousPaintingId => {
-    if (painting.id === previousPaintingId) {
+  getAdjacentPaintingsId(currentId) {
+    let index = Paintings.findIndex(p => p.id.toString() === currentId);
+    const previous = Paintings[index === 0 ? index : index - 1].id;
+    const next =
+      Paintings[index === Paintings.length - 1 ? index : index + 1].id;
+    return [previous, next];
+  }
+
+  renderArrow(direction) {
+    const id = direction === "left" ? this.previous : this.next;
+    if (this.painting.id === id) {
       return;
     } else {
       return (
-        <Link to={previousPaintingId.toString()}>
+        <Link to={id.toString()}>
           <FontAwesomeIcon
-            icon={["fas", "chevron-left"]}
+            icon={
+              direction === "left"
+                ? ["fas", "chevron-left"]
+                : ["fas", "chevron-right"]
+            }
             size="2x"
             style={{ color: "lightgray" }}
           />
         </Link>
       );
     }
-  };
+  }
 
-  const renderRightArrow = nextPaintingId => {
-    if (painting.id === nextPaintingId) {
-      return;
+  render() {
+    if (!this.painting) {
+      return <h1>No such painting</h1>;
     } else {
       return (
-        <Link to={nextPaintingId.toString()}>
-          <FontAwesomeIcon
-            icon={["fas", "chevron-right"]}
-            size="2x"
-            style={{ color: "lightgray" }}
-          />
-        </Link>
+        <div className="painting-details">
+          <div className="description">
+            <h2>{`"${this.painting.title}"`}</h2>
+            <h5>{this.painting.size[0] + "x" + this.painting.size[1]}</h5>
+            <h5>{this.painting.technique}</h5>
+            <h5>{this.painting.year}</h5>
+          </div>
+
+          <div className="image-navigation-wrapper">
+            <div className="painting-link">
+              {this.renderArrow("left")}
+            </div>
+            <img src={this.painting.image} alt={this.painting.title} />
+            <div className="painting-link">
+              {this.renderArrow("right")}
+            </div>
+          </div>
+        </div>
       );
     }
-  };
-
-  if (!painting) {
-    return <h1>No such painting</h1>;
-  } else {
-    const [previousPaintingId, nextPaintingId] = getAdjacentPaintingsId(
-      painting.id
-    );
-    return (
-      <div className="painting-details">
-        <div className="description">
-          <h2>{`"${painting.title}"`}</h2>
-          <h5>{painting.size[0] + "x" + painting.size[1]}</h5>
-          <h5>{painting.technique}</h5>
-          <h5>{painting.year}</h5>
-        </div>
-
-        <div className="image-navigation-wrapper">
-          <div className="painting-link">
-            {renderLeftArrow(previousPaintingId)}
-          </div>
-          <img src={painting.image} alt={painting.title} />
-          <div className="painting-link">
-            {renderRightArrow(nextPaintingId)}
-          </div>
-        </div>
-      </div>
-    );
   }
 }
 
