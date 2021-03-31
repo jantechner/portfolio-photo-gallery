a5e98f305f98e5fdeba511db613f26ac072b9656
jantechner <jantechner@live.com>
2021-03-31 10:54:09 +0000

    Deploying to gh-pages from  @ 7b54ba7a4b68ceb8003c4d79b2d09997d651eb68 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "malgorzatatechner.pl/precache-manifest.6fa5f207d799fa007cb2b57a9b1c3720.js"
+  "/precache-manifest.861c7b8c7456e56b4b6a6758c9758c66.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("malgorzatatechner.pl/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
